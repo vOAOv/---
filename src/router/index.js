b50e418a536ad16b6bef191f23f35d6631610217
dengrui <1359232264@qq.com>
2018-01-11 14:31:05 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import City from '@/page/city/City'
+import City from '@/pages/city'
 
 Vue.use(Router)
 
