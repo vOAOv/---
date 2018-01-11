e909ecca89bb9b566c1b071e950d31945dbaa364
dengrui <1359232264@qq.com>
2018-01-11 14:14:40 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import City from '@/page/city/City'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path: '/city',
+    	name: 'City',
+    	component: City
     }
   ]
 })
